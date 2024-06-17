c6074f17f536cb31a51f9e9a43f532664f958608
Anastasiia <iva.anastasiiaa@gmail.com>
2024-06-17 12:51:07 +0200

    fix: type ver  JS-sheet01-ex02 index.js

---
+++ JavaScript/sheet01/ex2/index.js
@@ -0,0 +1,16 @@
+var numero = 10;
+var cadena = "Hola, Mundo!";
+var booleano = true;
+
+// Mostrar los valores y tipos en la consola
+console.log("Variable 'numero':");
+console.log("Valor:", numero);
+console.log("Tipo:", typeof numero);
+
+console.log("Variable 'cadena':");
+console.log("Valor:", cadena);
+console.log("Tipo:", typeof cadena);
+
+console.log("Variable 'booleano':");
+console.log("Valor:", booleano);
+console.log("Tipo:", typeof booleano);
