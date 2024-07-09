56f441610f5620d213da8425e196a4463e04620b
Anastasiia <iva.anastasiiaa@gmail.com>
2024-07-09 13:19:31 +0200

    fix: JAVA/sheet01/ex02

---
+++ JAVA/sheet01/ex2/VariablesTipos.java
@@ -0,0 +1,23 @@
+package JAVA.sheet01.ex2;
+
+public class VariablesTipos {
+    public static void main(String[] args) {
+        int num;
+        double dob;
+        boolean bool;
+        char ch;
+        String str;
+
+        num = 4;
+        dob = 24E3d;
+        bool = true;
+        ch = 'A';
+        str = "hola";
+
+        System.out.println(num);
+        System.out.println(dob);
+        System.out.println(bool);
+        System.out.println(ch);
+        System.out.println(str);
+    }
+}
