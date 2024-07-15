cdaa776b8db5a301ec70789bb76f94a01a385013
Anastasiia <iva.anastasiiaa@gmail.com>
2024-07-15 12:52:46 +0200

    fix: JAVA/sheet02/ex1

--- JAVA/sheet01/ex07/Funciones.java
+++ JAVA/sheet01/ex07/Funciones.java
@@ -2,6 +2,12 @@ package JAVA.sheet01.ex07;
 
 public class Funciones {
     public static void main (String[] args) {
+<<<<<<< HEAD
+=======
+        // java Funciones 6 4
+        // args[0] es 6
+        // args[1] es 4
+>>>>>>> JAVA/sheet01/ex08
         int number1;
         int number2;
         int sum = number1 + number2;
---
+++ JAVA/sheet02/ex01/Persona.java
@@ -0,0 +1,23 @@
+package JAVA.sheet02.ex01;
+
+public class Persona {
+    String name;
+    int age;
+public Persona (String name, int age) {
+    this.name = name;
+    this.age = age;
+}
+
+public void mostrarDetalles () {
+    System.out.println("Nombre: " + name);
+    System.out.println("Edad: " + age);
+}
+
+
+public static void main(String[] args) {
+    Persona myObj = new Persona("Juan",25);
+    myObj.mostrarDetalles ();
+    
+} 
+
+}
