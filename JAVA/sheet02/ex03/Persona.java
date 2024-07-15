1f411a8fe05ec31a27123b134931c0e4c19a252a
Anastasiia <iva.anastasiiaa@gmail.com>
2024-07-15 13:53:30 +0200

    m: JAVA/sheet02/ex3

---
+++ JAVA/sheet02/ex03/Persona.java
@@ -0,0 +1,37 @@
+package JAVA.sheet02.ex02;
+
+public class Persona {
+    private String name;
+    private int age;
+public Persona (String name, int age) {
+    this.name = name;
+    this.age = age;
+}
+
+public void mostrarDetalles () {
+    System.out.println("Nombre: " + name);
+    System.out.println("Edad: " + age);
+}
+
+public void setName(String newName) {
+    this.name = newName;
+    } 
+
+public void setAge(int newAge) {
+   this.age = newAge;
+}
+
+public String getName () {
+    return name;
+}
+
+public int getAge() {
+    return age;
+}
+
+
+
+
+
+
+}
