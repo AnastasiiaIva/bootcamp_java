6cc93689a3457980bc242f59ba4dd744ee81f57c
Anastasiia <iva.anastasiiaa@gmail.com>
2024-07-16 13:15:08 +0200

    fix: JAVA/sheet02/ex6

---
+++ JAVA/sheet02/ex06/Animal.java
@@ -0,0 +1,8 @@
+package JAVA.sheet02.ex06;
+
+abstract class Animal {
+    public abstract void hacerSonido();
+    
+
+    
+}
---
+++ JAVA/sheet02/ex06/Main.java
@@ -0,0 +1,12 @@
+package JAVA.sheet02.ex06;
+
+public class Main {
+    public static void main(String[] args){
+        Perro peroObj = new Perro();
+        Gato gatoObj = new Gato();
+        peroObj.hacerSonido();
+        gatoObj.hacerSonido();
+
+    }
+    
+}
