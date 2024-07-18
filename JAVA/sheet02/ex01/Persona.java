1282c3a35b8a3468593b64e06a9526f57e617025
Anastasiia <iva.anastasiiaa@gmail.com>
2024-07-18 11:01:01 +0200

    Use Lombok

--- JAVA/sheet02/ex01/Persona.java
+++ JAVA/sheet02/ex01/Persona.java
@@ -3,25 +3,25 @@ package JAVA.sheet02.ex01;
 import lombok.Data;
 
 @Data
-
 public class Persona {
     String name;
     int age;
-/*public Persona (String name, int age) {
-    this.name = name;
-    this.age = age;*/
-}
 
-public void mostrarDetalles () {
-    System.out.println("Nombre: " + name);
-    System.out.println("Edad: " + age);
-}
+    public Persona (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-/*
-public static void main(String[] args) {
-    Persona myObj = new Persona("Juan",25);
-    myObj.mostrarDetalles ();
-    
-} */
+    public void mostrarDetalles () {
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
+    }
+
+    /*
+    public static void main(String[] args) {
+        Persona myObj = new Persona("Juan",25);
+        myObj.mostrarDetalles ();
+
+    } */
 
 }
